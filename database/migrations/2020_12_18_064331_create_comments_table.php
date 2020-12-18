89c19f054b460042f3e6522a236c064674ecee24
Markus <info@mo-webdesign.com>
2020-12-18 21:35:25 +0900

    Comment table fix

--- database/migrations/2020_12_18_064331_create_comments_table.php
+++ database/migrations/2020_12_18_064331_create_comments_table.php
@@ -15,8 +15,8 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('post_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
             $table->string('text');
             $table->timestamps();
         });
