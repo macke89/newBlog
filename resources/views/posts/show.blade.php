15d7ea296cf3677c5a9dbe7a5873f1380fee27e6
Markus <info@mo-webdesign.com>
2020-12-19 22:02:25 +0900

    Show comments per post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,11 +7,14 @@
             <h1>{{ $post->title }}</h1>
 
             <p>{{ $post->text }}</p>
+
             @foreach($comments as $comment)
-                <div class="card-body">
-                    <p class="card-text">{{ $comment->text }}</p>
-                    <a href="#" class="btn btn-primary">Answer</a>
-                </div>
+                @if($comment->post_id == $post->id)
+                    <div class="card-body">
+                        <p class="card-text">{{ $comment->text }}</p>
+                        <a href="#" class="btn btn-primary">Answer</a>
+                    </div>
+                @endif
             @endforeach
         </div>
     </div>
