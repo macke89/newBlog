c742aa32d7bbfa1ec4ba85f96dd6d1f173d4b2a3
macke89 <mark.opp.1989@gmail.com>
2021-01-02 01:23:26 +0900

    Post seeder (added photos)

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class PostFactory extends Factory
 {
@@ -24,11 +25,20 @@ class PostFactory extends Factory
     {
         $users = User::get('id');
 
+        $photos = [
+            'https://images.unsplash.com/photo-1609377232832-546f01970dea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609387116764-5d5be978a974?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609504373557-6721b7ba6584?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609505017222-3d23aa655e2f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
+            'https://images.unsplash.com/photo-1609504072070-b15e92363689?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+            'https://images.unsplash.com/photo-1525672716948-1f0bb9c49883?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80'
+        ];
+
         return [
             'title' => $this->faker->text('15'),
-//            'title' => $this->faker->randomElement($green),
             'user_id' => $this->faker->randomElement($users),
-            'text' => $this->faker->text('1000')
+            'text' => $this->faker->text('1000'),
+            'photo' => $this->faker->randomElement($photos)
         ];
     }
 }
