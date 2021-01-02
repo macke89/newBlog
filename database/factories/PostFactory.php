4a4188c327082aae8d29ff3b5872d717886c018e
macke89 <mark.opp.1989@gmail.com>
2021-01-02 11:24:20 +0900

    Forgot password

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -31,6 +31,23 @@ class PostFactory extends Factory
             'https://images.unsplash.com/photo-1609504373557-6721b7ba6584?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
             'https://images.unsplash.com/photo-1609505017222-3d23aa655e2f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
             'https://images.unsplash.com/photo-1609504072070-b15e92363689?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+            'https://images.unsplash.com/photo-1609414938124-9f334cb2e3ed?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609495058920-19c1829aec8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
+            'https://images.unsplash.com/photo-1609363990946-420bc7cdf51c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609418424268-97e8393fae45?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609410268177-ab7f58978947?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            'https://images.unsplash.com/photo-1609464455086-cef1e8deb2e2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=968&q=80',
+            'https://images.unsplash.com/photo-1609466726874-af7fb5db0d5d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+            'https://images.unsplash.com/photo-1609466686153-8da47ddda47b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80',
+            'https://images.unsplash.com/photo-1609518833016-e36fa49dbd55?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
+            'https://images.unsplash.com/photo-1609517405102-8e258999ef48?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=638&q=80',
+            'https://images.unsplash.com/photo-1609521217130-2a42077b8af3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80',
+            'https://images.unsplash.com/photo-1609521263047-f8f205293f24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
+            'https://images.unsplash.com/photo-1609452231591-f03a547a0fcc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
+            'https://images.unsplash.com/photo-1609511978060-305196d061c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+            'https://images.unsplash.com/photo-1609530477273-10457aa5d810?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80',
+            'https://images.unsplash.com/photo-1609520505555-8bb8d82184bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80',
+            'https://images.unsplash.com/photo-1609519408421-18e342a43080?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
             'https://images.unsplash.com/photo-1525672716948-1f0bb9c49883?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80'
         ];
 
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,54 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\PostController;
-use \App\Http\Controllers\CommentController;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\IndexController;
+    use App\Http\Controllers\PostController;
+    use \App\Http\Controllers\CommentController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Password;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+//    ABOUT PAGE
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
 
-Auth::routes();
+    Auth::routes();
 
-Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
-Route::get('/', [IndexController::class, 'index'])->name('index');
+//    DASHBOARD
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
 
-Route::resource('posts', PostController::class)->middleware('auth');
-Route::resource('comments', CommentController::class)->middleware('auth');
+//    INDEXPAGE
+    Route::get('/', [IndexController::class, 'index'])->name('index');
+
+//    FORGOT PASSWORD
+    Route::get('/forgot-password', function () {
+        return view('auth.passwords.email');
+    })->middleware('guest')->name('password.request');
+
+    Route::post('/forgot-password', function (Request $request) {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    })->middleware('guest')->name('password.email');
+
+
+    Route::resource('posts', PostController::class)->middleware('auth');
+    Route::resource('comments', CommentController::class)->middleware('auth');
