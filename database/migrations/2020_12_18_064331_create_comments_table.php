54fd85d1e5fd65bc0a007e47402819c02961df2c
macke89 <mark.opp.1989@gmail.com>
2021-01-22 08:24:42 +0900

    Updated syntax comment migration

--- database/migrations/2020_12_18_064331_create_comments_table.php
+++ database/migrations/2020_12_18_064331_create_comments_table.php
@@ -16,14 +16,11 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->integer('parent_id')->nullable();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('post_id');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
             $table->string('text', 400);
             $table->timestamps();
             $table->integer('votes')->default(0);
-
-            $table->foreign('post_id')->references('id')->on('posts');
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
