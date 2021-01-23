60c0594bffa225d9a8b541ed1cc3a42848a6505c
macke89 <mark.opp.1989@gmail.com>
2021-01-24 07:53:09 +0900

    Fixed voting for comments (fix)

--- database/migrations/2021_01_23_225139_add_votes_to_comments_table.php
+++ database/migrations/2021_01_23_225139_add_votes_to_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostVotesTable extends Migration
+class AddVotesToCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreatePostVotesTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_votes', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('post_id')->constrained();
-            $table->foreignId('user_id')->constrained();
-            $table->smallInteger('vote');
-            $table->timestamps();
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('votes')->default(0);
         });
     }
 
@@ -29,6 +25,8 @@ class CreatePostVotesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_votes');
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
     }
 }
